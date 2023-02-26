14da7c55e940ba73edd17c2b5801c190d591acf8
huzaifak08 <hk7928042@gmail.com>
2023-02-26 19:26:16 +0500

    Persistant bottom nav bar added Allhamdullillah

--- lib/view/dashboard.dart
+++ lib/view/dashboard.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:tech_media/res/color.dart';
 import 'package:tech_media/view_model/Provider/login_logout_providers.dart';
+import 'package:persistent_bottom_nav_bar/persistent_tab_view.dart';
 
 class DashboardScreen extends StatefulWidget {
   const DashboardScreen({super.key});
@@ -10,27 +12,60 @@ class DashboardScreen extends StatefulWidget {
 }
 
 class _DashboardScreenState extends State<DashboardScreen> {
+  // persistant controller:
+
+  final controller = PersistentTabController(initialIndex: 0);
+
+  List<Widget> _buildScreen() {
+    return [
+      Center(child: Text('Home')),
+      Center(child: Text('Chat')),
+      Center(child: Text('Add')),
+      Center(child: Text('Profile')),
+      Center(child: Text('Exit to App')),
+    ];
+  }
+
+  List<PersistentBottomNavBarItem> _navBarItem() {
+    return [
+      PersistentBottomNavBarItem(
+          icon: const Icon(Icons.home),
+          activeColorPrimary: AppColors.primaryIconColor,
+          inactiveIcon: Icon(Icons.home, color: Colors.grey.shade100)),
+      PersistentBottomNavBarItem(
+          icon: const Icon(Icons.chat),
+          activeColorPrimary: AppColors.primaryIconColor,
+          inactiveIcon: Icon(Icons.chat, color: Colors.grey.shade100)),
+      PersistentBottomNavBarItem(
+          icon: const Icon(Icons.add),
+          activeColorPrimary: AppColors.primaryIconColor,
+          inactiveIcon: Icon(Icons.add, color: Colors.grey.shade100)),
+      PersistentBottomNavBarItem(
+          icon: const Icon(Icons.account_circle),
+          activeColorPrimary: AppColors.primaryIconColor,
+          inactiveIcon:
+              Icon(Icons.account_circle, color: Colors.grey.shade100)),
+      PersistentBottomNavBarItem(
+          icon: const Icon(Icons.exit_to_app),
+          activeColorPrimary: AppColors.primaryIconColor,
+          inactiveIcon: Icon(Icons.exit_to_app, color: Colors.grey.shade100)),
+    ];
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Dashboard'),
-        actions: [
-          ChangeNotifierProvider(
-            create: (_) => LoginProvider(),
-            child: Consumer<LoginProvider>(
-              builder: (context, provider, child) {
-                return IconButton(
-                  onPressed: () {
-                    provider.logOut(context);
-                  },
-                  icon: const Icon(Icons.exit_to_app),
-                );
-              },
-            ),
-          )
-        ],
+    debugPrint('build');
+
+    return PersistentTabView(
+      context,
+      controller: controller,
+      screens: _buildScreen(),
+      items: _navBarItem(),
+      backgroundColor: AppColors.otpHintColor,
+      decoration: NavBarDecoration(
+        borderRadius: BorderRadius.circular(1),
       ),
+      navBarStyle: NavBarStyle.style15,
     );
   }
 }
